ce3ce3c2cc63428bc172317a842c7a6ee7c79742
allen-bui <allen.vbui@gmail.com>
2019-07-20 21:27:12 -0700

    completes seeding 10M records script

--- server/database/postgreSQL/seed.js
+++ server/database/postgreSQL/seed.js
@@ -1,13 +1,40 @@
+// const db = require('./app.js');
+
 const faker = require('faker');
-const db = require('./app.js');
-
-
+const pgp = require('pg-promise')();
+const connection = {
+  user: 'allen',
+  host: 'localhost',
+  database: 'sdc-reviews',
+  password: 'allenbui',
+  port: 5432,
+};
+const db = pgp(connection);
 const start = new Date().getTime();
 
+// create data
+const generateData = (numberOfDataPoints) => {
+  const data = [];
+  for (let i = 0; i < numberOfDataPoints; i++) {
+    data.push({ id: i, name: faker.lorem.sentence() });
+  }
+  return pgp.helpers.insert(data, ['id', 'name'], 'product');
+};
 
-for (let i = 0; i < 1000000; i++) {
-  db.insertRecords(i, faker.lorem.words(5));
-}
-
-const end = new Date().getTime();
-console.log(`[Execution Time]: ${end - start} milliseconds`);
+db.tx(async (t) => {
+  for (let i = 0; i < 1000; i++) {
+    await t.none(generateData(10000));
+    console.log(`Batch ${i + 1} of 1000 Completed`);
+  }
+})
+  .then(() =>
+    console.log('[Complete]: 10,000,000 records inserted successfully'),
+  )
+  .catch((e) => console.log(e))
+  .finally(() => {
+    const end = new Date().getTime();
+    console.log(
+      `[Execution Time]: ${Math.round((end - start) / 1000, 2)} seconds`,
+    );
+    pgp.end();
+  });
